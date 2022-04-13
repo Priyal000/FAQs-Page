481f8eab7ebd7016019ab0552fa6440de7a110b6
Priyal Jangid <86844296+Priyal000@users.noreply.github.com>
2022-04-13 21:46:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,197 @@
+function downKey() {
+    document.getElementById('down').style.display = "none"
+    document.getElementById('up').style.display = "block"
+    document.getElementById('answer1').style.display = "block"
+}
+function upKey() {
+    document.getElementById('down').style.display = "block"
+    document.getElementById('up').style.display = "none"
+    document.getElementById('answer1').style.display = "none"
+}
+
+function downKey2() {
+    document.getElementById('down1').style.display = "none"
+    document.getElementById('up1').style.display = "block"
+    document.getElementById('answer2').style.display = "block"
+}
+function upKey2() {
+    document.getElementById('down1').style.display = "block"
+    document.getElementById('up1').style.display = "none"
+    document.getElementById('answer2').style.display = "none"
+}
+
+function downKey3() {
+    document.getElementById('down2').style.display = "none"
+    document.getElementById('up2').style.display = "block"
+    document.getElementById('answer3').style.display = "block"
+}
+function upKey3() {
+    document.getElementById('down2').style.display = "block"
+    document.getElementById('up2').style.display = "none"
+    document.getElementById('answer3').style.display = "none"
+}
+
+function downKey4() {
+    document.getElementById('down3').style.display = "none"
+    document.getElementById('up3').style.display = "block"
+    document.getElementById('answer4').style.display = "block"
+}
+function upKey4() {
+    document.getElementById('down3').style.display = "block"
+    document.getElementById('up3').style.display = "none"
+    document.getElementById('answer4').style.display = "none"
+}
+
+function downKey5() {
+    document.getElementById('down4').style.display = "none"
+    document.getElementById('up4').style.display = "block"
+    document.getElementById('answer5').style.display = "block"
+}
+function upKey5() {
+    document.getElementById('down4').style.display = "block"
+    document.getElementById('up4').style.display = "none"
+    document.getElementById('answer5').style.display = "none"
+}
+
+function downKey6() {
+    document.getElementById('down5').style.display = "none"
+    document.getElementById('up5').style.display = "block"
+    document.getElementById('answer6').style.display = "block"
+}
+function upKey6() {
+    document.getElementById('down5').style.display = "block"
+    document.getElementById('up5').style.display = "none"
+    document.getElementById('answer6').style.display = "none"
+}
+
+function downKey7() {
+    document.getElementById('down6').style.display = "none"
+    document.getElementById('up6').style.display = "block"
+    document.getElementById('answer7').style.display = "block"
+}
+function upKey7() {
+    document.getElementById('down6').style.display = "block"
+    document.getElementById('up6').style.display = "none"
+    document.getElementById('answer7').style.display = "none"
+}
+
+function downKey8() {
+    document.getElementById('down7').style.display = "none"
+    document.getElementById('up7').style.display = "block"
+    document.getElementById('answer8').style.display = "block"
+}
+function upKey8() {
+    document.getElementById('down7').style.display = "block"
+    document.getElementById('up7').style.display = "none"
+    document.getElementById('answer8').style.display = "none"
+}
+
+function downKey9() {
+    document.getElementById('down8').style.display = "none"
+    document.getElementById('up8').style.display = "block"
+    document.getElementById('answer9').style.display = "block"
+}
+function upKey9() {
+    document.getElementById('down8').style.display = "block"
+    document.getElementById('up8').style.display = "none"
+    document.getElementById('answer9').style.display = "none"
+}
+
+function downKey10() {
+    document.getElementById('down9').style.display = "none"
+    document.getElementById('up9').style.display = "block"
+    document.getElementById('answer10').style.display = "block"
+}
+function upKey10() {
+    document.getElementById('down9').style.display = "block"
+    document.getElementById('up9').style.display = "none"
+    document.getElementById('answer10').style.display = "none"
+}
+
+function downKey11() {
+    document.getElementById('down10').style.display = "none"
+    document.getElementById('up10').style.display = "block"
+    document.getElementById('answer11').style.display = "block"
+}
+function upKey11() {
+    document.getElementById('down10').style.display = "block"
+    document.getElementById('up10').style.display = "none"
+    document.getElementById('answer11').style.display = "none"
+}
+
+function downKey12() {
+    document.getElementById('down11').style.display = "none"
+    document.getElementById('up11').style.display = "block"
+    document.getElementById('answer12').style.display = "block"
+}
+function upKey12() {
+    document.getElementById('down11').style.display = "block"
+    document.getElementById('up11').style.display = "none"
+    document.getElementById('answer12').style.display = "none"
+}
+
+function downKey13() {
+    document.getElementById('down12').style.display = "none"
+    document.getElementById('up12').style.display = "block"
+    document.getElementById('answer13').style.display = "block"
+}
+function upKey13() {
+    document.getElementById('down12').style.display = "block"
+    document.getElementById('up12').style.display = "none"
+    document.getElementById('answer13').style.display = "none"
+}
+
+function downKey14() {
+    document.getElementById('down13').style.display = "none"
+    document.getElementById('up13').style.display = "block"
+    document.getElementById('answer14').style.display = "block"
+}
+function upKey14() {
+    document.getElementById('down13').style.display = "block"
+    document.getElementById('up13').style.display = "none"
+    document.getElementById('answer14').style.display = "none"
+}
+
+function downKey15() {
+    document.getElementById('down14').style.display = "none"
+    document.getElementById('up14').style.display = "block"
+    document.getElementById('answer15').style.display = "block"
+}
+function upKey15() {
+    document.getElementById('down14').style.display = "block"
+    document.getElementById('up14').style.display = "none"
+    document.getElementById('answer15').style.display = "none"
+}
+
+function downKey16() {
+    document.getElementById('down15').style.display = "none"
+    document.getElementById('up15').style.display = "block"
+    document.getElementById('answer16').style.display = "block"
+}
+function upKey16() {
+    document.getElementById('down15').style.display = "block"
+    document.getElementById('up15').style.display = "none"
+    document.getElementById('answer16').style.display = "none"
+}
+
+function downKey17() {
+    document.getElementById('down16').style.display = "none"
+    document.getElementById('up16').style.display = "block"
+    document.getElementById('answer17').style.display = "block"
+}
+function upKey17() {
+    document.getElementById('down16').style.display = "block"
+    document.getElementById('up16').style.display = "none"
+    document.getElementById('answer17').style.display = "none"
+}
+
+function downKey18() {
+    document.getElementById('down17').style.display = "none"
+    document.getElementById('up17').style.display = "block"
+    document.getElementById('answer18').style.display = "block"
+}
+function upKey18() {
+    document.getElementById('down17').style.display = "block"
+    document.getElementById('up17').style.display = "none"
+    document.getElementById('answer18').style.display = "none"
+}
